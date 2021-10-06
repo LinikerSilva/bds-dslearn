5433bfda07dbf7d41304138e9823a7f3592acb04
liniker <linikergabrieldasilva@gmail.com>
2021-10-06 11:18:46 -0300

    Lesson, Content, Task

--- src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -30,7 +30,6 @@ public class User implements Serializable {
   @JoinTable(name = "tb_user_role",
       joinColumns = @JoinColumn(name = "user_id"),
       inverseJoinColumns = @JoinColumn(name = "role_id"))
-
   private Set<Role> roles = new HashSet<>();
 
   public User() {
